9b85af1db33776c1b084eb8a3d6629b8ed4aa8a8
ompks <rakhi.s@appdeveloperstudio.co.za>
2026-09-11 12:28:14 +0530

    formatted code

--- arrays and hashing/ContainsDuplicate.java
+++ arrays and hashing/ContainsDuplicate.java
@@ -9,18 +9,18 @@ class ContainsDuplicate {
 
         for (int num : nums) {
             // check if element is already in the hashset
-            if(uniqueNumbers.contains(num)){
+            if (uniqueNumbers.contains(num)) {
                 return true;
             }
             uniqueNumbers.add(num);
         }
 
-            return false; // No duplicates found
+        return false; // No duplicates found
     }
 
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,1};
+        int[] nums = {1, 2, 3, 1};
         ContainsDuplicate dc = new ContainsDuplicate();
         System.out.println(dc.containsDuplicate(nums));
     }
