e38512ad0156dbd926f907b66510970c21ba30e2
rakhisingh-27 <rakhi.s@appdeveloperstudio.co.za>
2026-09-11 12:25:17 +0530

    ContainsDuplicate solution

---
+++ arrays and hashing/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        // create hashset to store element from array
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+
+        // iterate through each element in the array
+
+        for (int num : nums) {
+            // check if element is already in the hashset
+            if(uniqueNumbers.contains(num)){
+                return true;
+            }
+            uniqueNumbers.add(num);
+        }
+
+            return false; // No duplicates found
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        ContainsDuplicate dc = new ContainsDuplicate();
+        System.out.println(dc.containsDuplicate(nums));
+    }
+
+}
